2533fa56aa9eba6c5513154183d3c2759770fbf8
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-05-22 14:24:28 +0700

    Add form for message and show incoming message in browser

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// Creating a New Project 17:27
+// Event Acknowledgements 7:12
 
 const path = require('path');
 const express = require('express');
@@ -19,33 +19,13 @@ app.use(express.static(publicPath));
 io.on('connection', function(socket) {
     console.log('New user connected');
 
-    // U/ tampilan pertama kali masuk
     socket.emit('newMessage', generateMessage('Admin', 'Wellcome to the chat app'));
-
-    // socket.emit('newMessage', {
-    //     from: 'Admin',
-    //     text: 'Welcome to the chat App',
-    //     createdAt: new Date().getTime()
-    // });
-    // ini u/ bila ada user masuk
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-    // socket.broadcast.emit('newMessage', {
-    //     from: 'Admin',
-    //     text: 'New user joined',
-    //     createdAt: new Date().getTime()
-    // });
 
-    // ini buat pesan baru
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
-
-        // ini buat models pesan
         io.emit('newMessage', generateMessage(message.from, message.text));
-        // io.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createAt: new Date().getTime()
-        // });
+        callback('This is from server');
     });
 
     socket.on('disconnect', function() {
